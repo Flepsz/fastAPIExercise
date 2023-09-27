fa96b022dd614c264628bcd9386908b55e2c497c
BrnGomes0 <brunownog05@gmail.com>
2023-09-27 11:29:39 -0300

    SecondCommit

---
+++ main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI
+from schemas import Item
+import requests
+import json
+from requests.structures import CaseInsensitiveDict
+
+app = FastAPI()
+
+
+# Creating a fake database
+fakeDatabase = {
+    1: {'task': 'Clean car'},
+    2: {'task': 'Write blog'},
+    3: {'task': 'Start stream'},
+    4: {'task': 'Do homework'},
+}
+
+
+@app.get("/")
+def getItems():
+    return fakeDatabase
+
+
+# Route with parameter
+@app.get("/{id}")
+def getItemId(id: int):
+    return fakeDatabase[id]
+
+#WithOut class
+# @app.post("/")
+# def addItem(task: str):
+#     newId = len(fakeDatabase.keys()) + 1 # Give the key of dicionarie and add one more
+#     fakeDatabase[newId] = {'task': task} # Create a new object
+#     return fakeDatabase
+
+#With a class
+@app.post("/")
+def addItem(item:Item):
+    newId = len(fakeDatabase.keys()) + 1 # Give the key of dicionarie and add one more
+    fakeDatabase[newId] = {'task': item.task} # Create a new object
+    return fakeDatabase
+
+# OBS: When the method pass many parameters, its better create a class
+
+@app.put("/{id}")
+def updateItem(id: int, item:Item):
+    fakeDatabase[id]['task'] = item.task 
+    return fakeDatabase
+
+@app.delete("/{id}")
+def deleteItem(id: int):
+    del fakeDatabase[id]
+    return fakeDatabase
+
+@app.get("/cnpj/{cnpj}")
+def getAPICNPJ(cnpj: str): 
+    url = f"https://publica.cnpj.ws/cnpj/{cnpj}"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        if response.status_code == 404:
+            return "CNPJ não encontrado."
+        
+        data = response.json()
+        
+        if 'cnpj_raiz' in data and 'razao_social' in data:
+            return data['cnpj_raiz']['razao_social']
+        else:
+            return "Dados não encontrados na resposta da API."
+    
+    except requests.exceptions.RequestException as e:
+        return f"Erro na solicitação HTTP: {e}"
+    
+    except ValueError as e:
+        return f"Erro na análise JSON: {e}"
