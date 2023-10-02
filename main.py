b5a1e196bef10fef282b214d05f3cf098fa76bec
BrnGomes0 <brunownog05@gmail.com>
2023-10-02 11:13:22 -0300

    Create function get cnpj fetchApi Exercise Two

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from schemas import Item, fakeDatabase
 import requests
-import json
 from requests.structures import CaseInsensitiveDict
+import json
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -28,14 +29,14 @@ def getItemId(id: int):
 @app.post("/")
 def addItem(item:Item):
     newId = len(fakeDatabase.keys()) + 1 # Give the key of dicionarie and add one more
-    fakeDatabase[newId] = {item.task: item.task} # Create a new object
+    fakeDatabase[newId] = {item.company: item.company} # Create a new object
     return fakeDatabase
 
 # OBS: When the method pass many parameters, its better create a class
 
 @app.put("/{id}")
 def updateItem(id: int, item:Item):
-    fakeDatabase[id]['task'] = item.task 
+    fakeDatabase[id]['company'] = item.company 
     return fakeDatabase
 
 @app.delete("/{id}")
@@ -45,24 +46,23 @@ def deleteItem(id: int):
 
 @app.get("/cnpj/{cnpj}")
 def getAPICNPJ(cnpj: str): 
-    url = f"https://publica.cnpj.ws/cnpj/{cnpj}"
-    
     try:
+        url = f'https://publica.cnpj.ws/cnpj/{cnpj}'
         response = requests.get(url)
         response.raise_for_status()
         
-        if response.status_code == 404:
-            return "CNPJ não encontrado."
-        
         data = response.json()
+        dataCompany = data['razao_social']
+        dataCNPJ = data['cnpj_raiz']
+
+        # Logic for add company in Task
         
-        if 'cnpj_raiz' in data and 'razao_social' in data:
-            return data['cnpj_raiz']['razao_social']
+        if 'razao_social':
+            return data['razao_social'], data['cnpj_raiz']
         else:
-            return "Dados não encontrados na resposta da API."
-    
+            return "Dados ausentes na resposta da API externa"
     except requests.exceptions.RequestException as e:
-        return f"Erro na solicitação HTTP: {e}"
-    
-    except ValueError as e:
-        return f"Erro na análise JSON: {e}"
+        raise HTTPException(status_code=500, detail="Erro na solicitação HTTP para a API externa")
+    except (KeyError, json.JSONDecodeError) as e:
+        raise HTTPException(status_code=500, detail="Erro ao processar a resposta da API externa")
+
